2ffbfb272df1fcf050a551f20e5b677803916469
Zair Ortiz <142334225+zairro@users.noreply.github.com>
2025-08-16 02:55:03 +0000

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    host: "0.0.0.0",
+    allowedHosts: ["localhost", "127.0.0.1", ".csb.app", ".codesandbox.io"],
     proxy: {
-      '/proxy': {
-        target: 'http://localhost:3001',
-        changeOrigin: true
-      }
-    }
-  }
-})
+      "/proxy": {
+        target: "http://localhost:3001",
+        changeOrigin: true,
+      },
+    },
+  },
+});
